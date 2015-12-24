c237fb754e1ca484c94cd6f1243d572dde41fcb0
Lon Sun <lon.sun@gmail.com>
2015-12-24 07:10:17 -0800

    Add postgre start/stop aliases.

--- config.fish
+++ config.fish
@@ -21,3 +21,7 @@ alias vi 'mvim'
 ### CPAY Work ###
 set -g -x PATH ~/code/cpay/forte/vendor/bin $PATH
 
+
+# Postgres (installed via homebrew)
+alias pg_start 'pg_ctl -D /usr/local/var/postgres/ -l /usr/local/var/postgres/server.log start'
+alias pg_stop 'pg_ctl -D /usr/local/var/postgres/ stop -s -m fast'
