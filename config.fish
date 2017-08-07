4e4ca6a3d8c809cf34df0d4b2b0e89aa77d32154
Lon Sun <lon.sun@gmail.com>
2017-08-06 19:28:52 -0700

    Add some aliases, android tools to PATH and some random stuff.

--- config.fish
+++ config.fish
@@ -4,6 +4,7 @@
 
 # Environment Vars
 set -g -x PATH /usr/local/bin $PATH
+set -g -x PATH /Users/lon/Library/Android/sdk/tools /Users/lon/Library/Android/sdk/platform-tools $PATH
 
 
 # Fish Greeting
@@ -11,7 +12,8 @@ set quotes "You don't worry about Planet Express, let me worry about blank." "In
 "Sweet zombie Jesus!" "60% of the time, it works everytime." \
 "[The city is being attacked by Lucy Liu robots] Now there’s a wave of destruction that’s easy on the eyes" "What is that, velvet?" \
 "It was a regulation date that ended in regulation dissapointment" \
-"[The court orders Bart to be emancipated and for Homer's wages to be garnished.] Mmmm...garnish."
+"[The court orders Bart to be emancipated and for Homer's wages to be garnished.] Mmmm...garnish." \
+"You are technically correct - the best kind of correct."
 
 function fish_greeting
   # pick a random quote...
@@ -25,9 +27,13 @@ function fish_greeting
 end
 
 
+# iTerm2 shell integration
+source ~/.iterm2_shell_integration.fish
+
 # Aliases
 alias rm 'rm -i'
 alias vi 'mvim'
+alias updatedb '/usr/libexec/locate.updatedb'
 
 ## Postgres (installed via homebrew)
 alias start_pg 'pg_ctl -D /usr/local/var/postgres/ -l /usr/local/var/postgres/server.log start'
@@ -35,8 +41,6 @@ alias stop_pg 'pg_ctl -D /usr/local/var/postgres/ stop -s -m fast'
 alias start_pg95 'pg_ctl -D /usr/local/var/postgres-9.5/ -l /usr/local/var/postgres/server.log start'
 alias stop_pg95 'pg_ctl -D /usr/local/var/postgres-9.5/ stop -s -m fast'
 
-# iTerm2 shell integration
-source ~/.iterm2_shell_integration.fish
 
 #                 #
 ### SpotOn Work ###
